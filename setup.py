dcc3297dd9fca0d9caf107e911c062b917caec46
Maor Kleinberger <kmaork@gmail.com>
2020-07-11 03:29:34 +0300

    fixed packaging

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from pathlib import Path
-from setuptools import setup
-from distutils.core import Extension
+from setuptools import setup, Extension
 
-PROJECT_ROOT = Path(__file__).parent
-INJECTOR_DIR = PROJECT_ROOT / 'injector'
+PROJECT_ROOT = Path(__file__).parent.resolve()
+LIBINJECTOR_DIR = PROJECT_ROOT / 'injector'
+LIBINJECTOR_SRC = LIBINJECTOR_DIR / 'src' / 'linux'
 
 libinjector = Extension('libinjector',
-                        include_dirs=[str(INJECTOR_DIR / 'include')],
-                        sources=[str(INJECTOR_DIR / 'src' / 'linux' / c) for c in
-                                 ('elf.c', 'injector.c', 'ptrace.c', 'remote_call.c', 'util.c')])
+                        sources=[str(c.relative_to(PROJECT_ROOT)) for c in LIBINJECTOR_SRC.iterdir()
+                                 if c.suffix == '.c'],
+                        include_dirs=[str(LIBINJECTOR_DIR.relative_to(PROJECT_ROOT) / 'include')])
 
 setup(
     ext_modules=[libinjector],
-    entry_points={"console_scripts": ["inject=pyinjector:main"]},
+    entry_points={"console_scripts": ["inject=pyinjector:main"]}
 )
