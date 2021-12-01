f6db2a4a7e1ddbe425cd074c751265ff2eb76aa0
Maor Kleinberger <kmaork@gmail.com>
2021-12-01 23:41:28 +0200

    Refactor setup.py and use the setuptools Extension class

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from sys import platform
 from pathlib import Path
 from setuptools import setup, Extension
-from distutils.extension import Extension
 
 WINDOWS = platform == 'win32'
 PROJECT_ROOT = Path(__file__).parent.resolve()
 LIBINJECTOR_DIR = PROJECT_ROOT / 'injector'
 LIBINJECTOR_SRC = LIBINJECTOR_DIR / 'src' / ('windows' if WINDOWS else 'linux')
 LIBINJECTOR_WRAPPER = PROJECT_ROOT / 'libinjector.c'
+SOURCES = [str(c.relative_to(PROJECT_ROOT))
+           for c in [LIBINJECTOR_WRAPPER, *LIBINJECTOR_SRC.iterdir()]
+           if c.suffix == '.c']
 
 libinjector = Extension('pyinjector.libinjector',
-                        sources=[str(c.relative_to(PROJECT_ROOT))
-                                 for c in [LIBINJECTOR_WRAPPER, *LIBINJECTOR_SRC.iterdir()]
-                                 if c.suffix == '.c'],
+                        sources=SOURCES,
                         include_dirs=[str(LIBINJECTOR_DIR.relative_to(PROJECT_ROOT) / 'include')],
                         export_symbols=['injector_attach', 'injector_inject', 'injector_detach', 'injector_error'],
                         define_macros=[('EM_AARCH64', '183')])  # Needed on CentOS for some reason
